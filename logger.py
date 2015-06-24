0003c737c57cf399355bf6f6326c9113b7fbe24a
ofalvai <ofalvai@gmail.com>
2015-06-24 18:03:38 +0200

    New database schema, rounding, etc

--- logger.py
+++ logger.py
@@ -8,21 +8,22 @@ sensor = Adafruit_DHT.DHT22
 pin = 4
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-# print 'Temp: {0:0.1f} *C, Humidity: {1:0.1f}%'.format(temperature, humidity)
+print 'Temp: {0:0.1f} *C, Humidity: {1:0.1f}%'.format(temperature, humidity)
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 # Cron-bol futtatva nem jo a relative path
 
 try:
-    db = sqlite3.connect(os.path.join(dir_path, 'raspi-weather-test.db'))
+    db = sqlite3.connect(os.path.join(dir_path, 'raspi-weather.db'))
     c = db.cursor()
 
-    args = ['now', 'localtime', temperature, humidity]
-    c.execute('INSERT INTO indoor (timestamp, temp, hum) VALUES (datetime(?, ?), ?, ?)', args)
+    args = ['now', 'localtime', round(temperature, 1), int(humidity)]
+    c.execute('INSERT INTO indoor (timestamp, temperature, humidity) VALUES (datetime(?, ?), ?, ?)', args)
     db.commit()
     db.close()
 except sqlite3.Error as err:
     f = open(os.path.join(dir_path, 'sensor.log'), 'a')
+    print(str(err))
     f.write(str(err))
     f.write('\n')
     f.close()
--- sensor.py
+++ sensor.py
@@ -7,4 +7,4 @@ sensor = Adafruit_DHT.DHT22
 pin = 4
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-print json.dumps({'temperature': round(temperature, 1), 'humidity': round(humidity, 1)});
+print json.dumps({'temperature': round(temperature, 1), 'humidity': int(humidity)});
