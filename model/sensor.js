93d2d8350f3959f809249e64a13fb87fa7bc6060
ofalvai <ofalvai@gmail.com>
2015-07-02 22:00:18 +0200

    sensor.js refactor

--- model/sensor.js
+++ model/sensor.js
@@ -1,21 +1,22 @@
 var execSync = require('child_process').execSync;
 var path = require('path');
-var sensor = {};
 
-sensor.getCurrent = function() {
-    var result = execSync('./sensor.py').toString().split('\n');
-    if(result[0] == "error") {
-        return {
-            success: false,
-            error: result[1]
+var sensor = {
+    getCurrent: function() {
+        var result = execSync('./sensor.py').toString().split('\n');
+        if(result[0] == "error") {
+            return {
+                success: false,
+                error: result[1]
+            }
+        } else {
+            return {
+                success: true,
+                temperature: result[0],
+                humidity: result[1]
+            };
         }
-    } else {
-        return {
-            success: true,
-            temperature: result[0],
-            humidity: result[1]
-        };
     }
-}
+};
 
 module.exports = sensor;
