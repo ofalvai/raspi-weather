646a89c94f8aca4bb91c3387b97872966391a7ce
ofalvai <ofalvai@gmail.com>
2015-07-03 18:47:25 +0200

    Bootstrap + theme + first design steps

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
